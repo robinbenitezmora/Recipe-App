93985f643644b6d8730fdc2095430f7543cf452c
robinbenitezmora <robinbenitez1990@gmail.com>
2022-12-20 12:04:19 -0500

    Define controllers and initial form for the recipe_foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'recipe_foods/index'
+  get 'recipe_foods/show'
+  get 'recipe_foods/new'
+  get 'recipe_foods/edit'
+  get 'recipe_foods/create'
+  get 'recipe_foods/update'
+  get 'recipe_foods/destroy'
   resources :users
   resources :recipes
   resources :foods
