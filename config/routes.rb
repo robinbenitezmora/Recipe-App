848db9c0f69476fb5631c75fe469e6f87a7d5845
robinbenitezmora <robinbenitez1990@gmail.com>
2022-12-20 07:48:46 -0500

    Create 'recipes_controller.rb' file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
