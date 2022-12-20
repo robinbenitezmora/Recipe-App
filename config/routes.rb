43409bd34a54ef1092f8c505f0bb5edd2baf5294
robinbenitezmora <robinbenitez1990@gmail.com>
2022-12-20 10:46:28 -0500

    Define controllers and initial form for the foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'foods/index'
+  get 'foods/show'
   resources :users
   resources :recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
