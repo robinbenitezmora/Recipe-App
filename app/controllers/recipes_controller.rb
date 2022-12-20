186bfcfd951b5f5487f27560577c5f954b1e5beb
robinbenitezmora <robinbenitez1990@gmail.com>
2022-12-20 18:42:38 -0500

    Modify again several files

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ class RecipesController < ApplicationController
   before_action :set_recipe, only: %i[show edit update destroy]
 
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.where(user_id: current_user.id)
   end
 
   def show; end
