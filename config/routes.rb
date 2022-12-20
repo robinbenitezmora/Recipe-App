0c509889c21b380d764827d97ce9caf6e8b7f4c8
robinbenitezmora <robinbenitez1990@gmail.com>
2022-12-20 12:35:41 -0500

    Complete controllers and views for 'recipe_foods'

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'recipe_foods/index'
-  get 'recipe_foods/show'
-  get 'recipe_foods/new'
-  get 'recipe_foods/edit'
-  get 'recipe_foods/create'
-  get 'recipe_foods/update'
-  get 'recipe_foods/destroy'
   resources :users
   resources :recipes
   resources :foods
+  resources :recipe_foods
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
