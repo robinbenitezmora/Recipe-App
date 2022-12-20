7e66164630ec8dc89d63a6e2895f5fb7c5bb44cc
robinbenitezmora <robinbenitez1990@gmail.com>
2022-12-20 11:34:38 -0500

    Complete tests in 'spec.rb' files

--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -1,18 +1,115 @@
 require 'rails_helper'
 
-RSpec.describe "Foods", type: :request do
+RSpec.describe "/foods", type: :request do
+  let(:valid_attributes) {
+    skip("Add a hash of attributes valid for your model")
+  }
+
+  let(:invalid_attributes) {
+    skip("Add a hash of attributes invalid for your model")
+  }
+
   describe "GET /index" do
-    it "returns http success" do
-      get "/foods/index"
-      expect(response).to have_http_status(:success)
+    it "renders a successful response" do
+      Food.create! valid_attributes
+      get foods_url
+      expect(response).to be_successful
     end
   end
 
   describe "GET /show" do
-    it "returns http success" do
-      get "/foods/show"
-      expect(response).to have_http_status(:success)
+    it "renders a successful response" do
+      food = Food.create! valid_attributes
+      get food_url(food)
+      expect(response).to be_successful
     end
   end
 
+  describe "GET /new" do
+    it "renders a successful response" do
+      get new_food_url
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET /edit" do
+    it "renders a successful response" do
+      food = Food.create! valid_attributes
+      get edit_food_url(food)
+      expect(response).to be_successful
+    end
+  end
+
+  describe "POST /create" do
+    context "with valid parameters" do
+      it "creates a new Food" do
+        expect {
+          post foods_url, params: { food: valid_attributes }
+        }.to change(Food, :count).by(1)
+      end
+
+      it "redirects to the created food" do
+        post foods_url, params: { food: valid_attributes }
+        expect(response).to redirect_to(food_url(Food.last))
+      end
+    end
+
+    context "with invalid parameters" do
+      it "does not create a new Food" do
+        expect {
+          post foods_url, params: { food: invalid_attributes }
+        }.to change(Food, :count).by(0)
+      end
+
+      it "renders a response with 422 status" do
+        post foods_url, params: { food: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe "PATCH /update" do
+    context "with valid parameters" do
+      let(:new_attributes) {
+        skip("Add a hash of attributes valid for your model")
+      }
+
+      it "updates the requested food" do
+        food = Food.create! valid_attributes
+        patch food_url(food), params: { food: new_attributes }
+        food.reload
+        skip("Add assertions for updated state")
+      end
+
+      it "redirects to the food" do
+        food = Food.create! valid_attributes
+        patch food_url(food), params: { food: new_attributes }
+        food.reload
+        expect(response).to redirect_to(food_url(food))
+      end
+    end
+
+    context "with invalid parameters" do
+      it "renders a response with 422 status" do
+        food = Food.create! valid_attributes
+        patch food_url(food), params: { food: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe "DELETE /destroy" do
+    it "destroys the requested food" do
+      food = Food.create! valid_attributes
+      expect {
+        delete food_url(food)
+      }.to change(Food, :count).by(-1)
+    end
+
+    it "redirects to the foods list" do
+      food = Food.create! valid_attributes
+      delete food_url(food)
+      expect(response).to redirect_to(foods_url)
+    end
+  end
 end
--- spec/views/foods/index.html.erb_spec.rb
+++ spec/views/foods/index.html.erb_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe "foods/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "foods/index", type: :view do
+  before(:each) do
+    assign(:foods, [
+      Food.create!(
+        name: "Name",
+        measurement_unit: "Measurement Unit",
+        price: "9.99",
+        quantity: "9.99",
+        user: nil
+      ),
+      Food.create!(
+        name: "Name",
+        measurement_unit: "Measurement Unit",
+        price: "9.99",
+        quantity: "9.99",
+        user: nil
+      )
+    ])
+  end
+
+  it "renders a list of foods" do
+    render
+    cell = Rails::VERSION::STRING >= '6.1' ? 'div>p' : 'tr>td'
+    assert_select "#{cell}", text: "Name".to_s, count: 2
+    assert_select "#{cell}", text: "Measurement Unit".to_s, count: 2
+    assert_select "#{cell}", text: "9.99".to_s, count: 2
+    assert_select "#{cell}", text: "9.99".to_s, count: 2
+    assert_select "#{cell}", text: nil.to_s, count: 2
+  end
 end
