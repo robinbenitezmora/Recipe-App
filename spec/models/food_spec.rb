96fef7b1b64344e376655c67a602444b5499781c
Oluyemi Paul <oluola96@gmail,com>
2022-12-21 23:59:10 +0100

    resolve linters err

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  subject { Food.new(name: 'Potatoe', price: 15.25, measurementunit: 'Kg', user_id: 1)}
+  subject { Food.new(name: 'Potatoe', price: 15.25, measurementunit: 'Kg', user_id: 1) }
 
   before { subject.save }
 
