203bb56211a65afd64192ceb8303d0fcf661e535
robinbenitezmora <robinbenitez1990@gmail.com>
2022-12-20 20:18:47 -0500

    Add tess in 'food_spec.rb' and 'recipe_spec.rb' files

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Food.new(name: 'Potatoe', price: 15.25, measurement_unit: 'Kg', user_id: 1)}
+
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'price should be present' do
+    subject.price = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'measurement_unit should be present' do
+    subject.measurement_unit = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'user_id should be a float greater or equal than zero' do
+    subject.user_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'price must be a float greater or equal than zero' do
+    subject.price = -1
+    expect(subject).to_not be_valid
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
 end
