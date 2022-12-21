42ae51245b70a5f1f3dfd5fa589b5c6e89bf9f2d
robinbenitezmora <robinbenitez1990@gmail.com>
2022-12-20 19:58:07 -0500

    Add validation to 'recipes' and 'food' files

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,8 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
+
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
