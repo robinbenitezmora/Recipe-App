8d21530fe685204fafc1137f2f439ff48e8e77c9
Oluyemi Paul <oluola96@gmail,com>
2022-12-22 21:57:13 +0100

    add inventory routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   resources :recipes
   resources :foods
   resources :recipe_foods
+
+  # Defines the routes for the inventory and inventory_foods controller
+  resources :inventories, except: %i[update] do
+    resources :inventory_foods, only: %i[new create destroy]
+  end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
