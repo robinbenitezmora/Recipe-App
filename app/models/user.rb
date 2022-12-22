8b8f6ed2da6c625dc5c95adc30927260f34e7c33
Oluyemi Paul <oluola96@gmail,com>
2022-12-22 10:54:15 +0100

    add association to user and inventory

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :recipes
   has_many :foods
+  has_many :inventories, foreign_key: :user_id, dependent: :destroy
 
   validates :name, presence: true
 end
