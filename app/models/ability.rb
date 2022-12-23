06a27be38a0ada238e42c00189958f4fbb4d1ca2
Edith oga <vigehi2017@gmail.com>
2022-12-23 13:23:14 +0300

    Fix offences

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
+    # user ||= User.new # guest user (not logged i
     can :read, :all
   end
 end
