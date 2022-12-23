e24e6e727573708d891d174b9ca77d6f4004805b
Edith oga <vigehi2017@gmail.com>
2022-12-23 14:14:13 +0300

    updates

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -8,24 +8,37 @@ class RecipeFoodsController < ApplicationController
   def show; end
 
   def new
+    @recipe = Recipe.find(params[:id])
     @recipe_food = RecipeFood.new
   end
-
   def edit; end
 
   def create
-    @recipe_food = RecipeFood.new(recipe_food_params)
+    food_list = params[:recipe_food][:food_list]
+    food_list = food_list.drop(1)
+    food_list.each do |food|
+      next unless RecipeFood.where(food_id: food.to_i, recipe_id: params[:id]).blank?
 
-    respond_to do |format|
-      if @recipe_food.save
-        format.html { redirect_to recipe_food_url(@recipe_food), notice: 'Recipe food was successfully created.' }
-        format.json { render :show, status: :created, location: @recipe_food }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @recipe_food.errors, status: :unprocessable_entity }
-      end
+      new_recipe_food = RecipeFood.new(food_id: food.to_i, quantity: params[:recipe_food][:quantity],
+                                       recipe_id: params[:id])
+      new_recipe_food.save
     end
+    redirect_to user_recipe_path(params[:id]), flash: { success: 'Recipe food has been added successfully!' }
   end
+
+  # def create
+  #   @recipe_food = RecipeFood.new(recipe_food_params)
+
+  #   respond_to do |format|
+  #     if @recipe_food.save
+  #       format.html { redirect_to recipe_food_url(@recipe_food), notice: 'Recipe food was successfully created.' }
+  #       format.json { render :show, status: :created, location: @recipe_food }
+  #     else
+  #       format.html { render :new, status: :unprocessable_entity }
+  #       format.json { render json: @recipe_food.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   def update
     respond_to do |format|
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,86 +1,46 @@
-# class RecipesController < ApplicationController
-#   before_action :set_recipe, only: %i[show edit update destroy]
-
-#   def index
-#     @recipes = Recipe.where(user_id: current_user.id)
-#   end
-
-#   def show; end
-
-#   def new
-#     @recipe = Recipe.new
-#   end
-
-#   def edit; end
-
-#   def update
-#     respond_to do |format|
-#       if @recipe.update(recipe_params)
-#         format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully updated.' }
-#         format.json { render :show, status: :ok, location: @recipe }
-#       else
-#         format.html { render :edit, status: :unprocessable_entity }
-#         format.json { render json: @recipe.errors, status: :unprocessable_entity }
-#       end
-#     end
-#   end
-
-#   def destroy
-#     @recipe.destroy
-#     respond_to do |format|
-#       format.html { redirect_to recipes_url, notice: 'Recipe was successfully destroyed.' }
-#       format.json { head :no_content }
-#     end
-#   end
-
-#   private
-
-#   def set_recipe
-#     @recipe = Recipe.find(params[:id])
-#   end
-
-#   def recipe_params
-#     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
-#   end
-# end
 class RecipesController < ApplicationController
-  def load_and_authorize_resource
-    load_resource
-    authorize_resource
-  end
-
-  before_action :set_recipe, only: %i[show destroy]
-  before_action :authenticate_user!, only: %i[new create destroy]
+  before_action :set_recipe, only: %i[show edit update destroy]
 
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.where(user_id: current_user.id)
+  end
+
+  def show
+    @foods = current_user.foods
+    @inventories = Inventory.all
+    @recipe = current_user.recipes.includes(:recipe_foods).find(params[:id])
+    @recipes = Recipe.find(params[:id])
   end
 
   def new
-    # @user = User.find(params[:user_id])
-    @recipes = Recipe.new
+    @user = User.find(params[:user_id])
+    @recipe = Recipe.new
   end
 
-  # def create
-  #   @recipe = Recipe.new(recipe_params)
-  #   @recipe.user_id = current_user.id
+  def edit; end
 
-  #   if @recipe.save
-  #     redirect_to user_recipes_path
-  #   else
-  #     render :new
-  #   end
-  # end
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.user_id = current_user.id
 
+    respond_to 
+    if @recipe.save
+      format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully created.' }
+      format.json { render :show, status: :created, location: @recipe }
+    else
+      format.html { render :new, status: :unprocessable_entity }
+      format.json { render json: @recipe.errors, status: :unprocessable_entity }
+    end
+
+  end
+
+  def update
     respond_to do |format|
-      if @recipe.save
-        format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully created.' }
-        format.json { render :show, status: :created, location: @recipe }
+      if @recipe.update(recipe_params)
+        format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully updated.' }
+        format.json { render :show, status: :ok, location: @recipe }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @recipe.errors, status: :unprocessable_entity }
       end
     end
@@ -88,11 +48,10 @@ class RecipesController < ApplicationController
 
   def destroy
     @recipe.destroy
-    redirect_to user_recipes_path
-  end
-
-  def show
-    @foods = @recipe.recipe_foods.includes(:food)
+    respond_to do |format|
+      format.html { redirect_to recipes_url, notice: 'Recipe was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
@@ -102,6 +61,6 @@ class RecipesController < ApplicationController
   end
 
   def recipe_params
-    params.require(:recipe).permit(:name, :description, :preparation_time, :cooking_time, :public)
+    params.require(:recipe).permit(:name, :preparationtime, :cookingtime, :description, :public)
   end
 end
