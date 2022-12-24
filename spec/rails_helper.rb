e5c264c28b31ed5270efe0887b57bbe07321542b
Oluyemi Paul <oluola96@gmail.com>
2022-12-24 07:34:32 +0100

    Update rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,7 +42,7 @@ Capybara.javascript_driver = :selenium_chrome
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = "#{Rails.root}/spec/fixtures"
   # config.include FactoryBot::Syntax::Methods
 
   # config.include FactoryBot::Syntax::Methods
